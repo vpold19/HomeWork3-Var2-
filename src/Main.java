57ede19fcd883c490be8dd90d4c77687563d6768
vadim <vadimpoldolin19@gmail.com>
2023-02-14 19:18:22 +0300

    Задачи 1-3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,77 @@
 public class Main {
-    public static void main(String[] args)
-    {
-        System.out.println("Hello world!");
-    }
+     public static void main(String[] args) {
+          task1();
+          task2();
+          task3();
+          task4();
+          task5();
+          task6();
+          task7();
+          task8();
+     }
+
+     public static void task1() {
+          System.out.println("Задача 1");
+          byte a = 1;
+          System.out.println("Значение переменной byte с типом a равно " + a);
+          short b = 200;
+          System.out.println("Значение переменной short с типом b равно " + b);
+          int i = 3000;
+          System.out.println("Значение переменной int с типом i равно " + i);
+          long d = 5000000000L;
+          System.out.println("Значение переменной long с типом d равно " + d);
+          float e = 3f;
+          System.out.println("Значение переменной float с e типом равно " + e);
+          double g = 4d;
+          System.out.println("Значение переменной double с g типом равно " + g);
+     }
+
+     public static void task2() {
+          System.out.println("Задача 2");
+          double a = 27.12d;
+          System.out.println("double = " + a);
+          long b= 987678965549L;
+          System.out.println("long = " +b);
+          double c=2.786d;
+          System.out.println("double= " + c);
+          short d= 569;
+          System.out.println("short = " + d);
+          short e=-159;
+          System.out.println("short = " + e);
+          int f=27897;
+          System.out.println("int = " + f);
+          byte g=67;
+          System.out.println("byte = " + g);
+     }
+
+     public static void task3() {
+          System.out.println("Задача 3");
+          byte lp = 23;
+          byte as = 27;
+          byte ea = 30;
+          short allPaper= 480;
+          int allStudents= (lp+as+ea);
+          int paperOneStudent= (allPaper/allStudents);
+          System.out.println("На каждого ученика рассчитано "+ paperOneStudent + " листов бумаги");
+     }
+
+     public static void task4() {
+          System.out.println("Задача 4");
+     }
+
+     public static void task5() {
+          System.out.println("Задача 5");
+     }
+
+     public static void task6() {
+          System.out.println("Задача 6");
+     }
+
+     public static void task7() {
+          System.out.println("Задача 7");
+     }
+
+     public static void task8() {
+          System.out.println("Задача 8");
+     }
 }
