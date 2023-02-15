ed74f665b647efa71cd5d8950684741292169bac
vadim <vadimpoldolin19@gmail.com>
2023-02-15 22:38:47 +0300

    Задача 5-8

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+
 public class Main {
      public static void main(String[] args) {
           task1();
@@ -70,17 +71,68 @@ public class Main {
 
      public static void task5() {
           System.out.println("Задача 5");
+          int paintCans=120;
+          byte whiteCansOneClass=2;
+          byte brownCansOneClass=4;
+          int cansOneClass= (whiteCansOneClass+brownCansOneClass);
+          int classOfSchool=( paintCans/cansOneClass);
+          int allWhiteCans=(classOfSchool*whiteCansOneClass);
+          int allBrownCans=(classOfSchool*brownCansOneClass);
+          System.out.println("В школе, где " + classOfSchool + " классов, нужно " + allWhiteCans + " банок белой краски и " + allBrownCans + " банок коричневой краски");
      }
 
      public static void task6() {
           System.out.println("Задача 6");
+          int ban = 5 * 80;
+          System.out.println(ban + " гр бананов");
+          int milk = 105*2;
+          System.out.println(milk + " гр молока");
+          int iceCream = 100 *2;
+          System.out.println(iceCream + " гр мороженного");
+          int eggs=4*70;
+          System.out.println(eggs + " гр яиц");
+          int shakeGr=(ban + milk + iceCream + eggs );
+          System.out.println(shakeGr + " гр коктейла");
+          double shakeKg = shakeGr/1000;
+          System.out.println(shakeKg + " кг в коктейле");
      }
 
      public static void task7() {
           System.out.println("Задача 7");
+          int sevenKgInGr=7*1000;
+          int lowWeight=sevenKgInGr/250;
+          int highWeight=sevenKgInGr/500;
+          System.out.println(lowWeight + "  дней уйдет на похудение, если спортсмен будет терять каждый день по 250 грамм");
+          System.out.println(highWeight + " дней уйдет на похудение, если спортсмен будет терять каждый день по 500 грамм");
+          int midWeight =( lowWeight + highWeight) /2;
+          System.out.println(midWeight + " дней может потребоваться в среднем, чтобы добиться результата похудения");
      }
 
      public static void task8() {
           System.out.println("Задача 8");
+          int mariaSalary=67760;
+          double mariaBonus= mariaSalary*0.1;
+          double mariaSalaryMonth = 67760 + mariaBonus;
+          double mariaAnualSalaryBefore= 67760*12;
+          double mariaAnualSalaryAfter= mariaSalaryMonth*12;
+          double mariaSalaryDiff = mariaAnualSalaryAfter-mariaAnualSalaryBefore;
+          System.out.println("Маша теперь получает " + mariaSalaryMonth + " рублей в месяц");
+          System.out.println("Годовой доход Марии вырос на " + mariaSalaryDiff + " рублей");
+          int kristinaSalary=76230;
+          double kristinaBonus= kristinaSalary*0.1;
+          double kristinaSalaryMonth = 76230 + kristinaBonus;
+          double kristinaAnualSalaryBefore= 76230*12;
+          double kristinaAnualSalaryAfter= kristinaSalaryMonth*12;
+          double kristinaSalaryDiff = kristinaAnualSalaryAfter-kristinaAnualSalaryBefore;
+          System.out.println("Кристина теперь получает " + kristinaSalaryMonth + " рублей в месяц");
+          System.out.println("Годовой доход Марии вырос на " + kristinaSalaryDiff + " рублей");
+          int denisSalary=83600;
+          double denisBonus= denisSalary*0.1;
+          double denisSalaryMonth = 83600 + denisBonus;
+          double denisAnualSalaryBefore= 83600*12;
+          double denisAnualSalaryAfter= denisSalaryMonth*12;
+          double denisSalaryDiff = denisAnualSalaryAfter-denisAnualSalaryBefore;
+          System.out.println("Денис теперь получает " + denisSalaryMonth + " рублей в месяц");
+          System.out.println("Годовой доход Марии вырос на " + denisSalaryDiff + " рублей");
      }
 }
